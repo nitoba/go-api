e64b4396399a9a3d037fc3eef3d9e11eac590626
Bruno Alves <nito.dev@gmail.com>
2024-02-07 02:01:33 -0300

    refactor: format as json style

--- test/cryptography/fake_encrypter.go
+++ test/cryptography/fake_encrypter.go
@@ -14,7 +14,7 @@ func (f *FakeEncrypter) Encrypt(payload map[string]interface{}) string {
 func createKeyValuePairs(m map[string]interface{}) string {
 	b := new(bytes.Buffer)
 	for key, value := range m {
-		fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
+		fmt.Fprintf(b, "\"%s\":\"%s\"\n", key, value)
 	}
 	return b.String()
 }
